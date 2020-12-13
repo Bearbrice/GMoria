0ded31fbeeb156f37f1570ac22ce4346e0b12f38
Bearbrice <berclazpro@gmail.com>
2020-12-13 17:21:00 +0100

    JSON to YAML for language

--- lib/app/utils/app_localizations.dart
+++ lib/app/utils/app_localizations.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:yaml/yaml.dart';
 
 class AppLocalizations {
   final Locale locale;
@@ -19,20 +19,36 @@ class AppLocalizations {
     return Localizations.of<AppLocalizations>(context, AppLocalizations);
   }
 
-  Map<String, String> _localizedStrings;
+  var _localizedStrings;
 
+  ///YAML
   Future<bool> load() async {
-    // Load the language JSON file from the "lang" folder
+    // Load the language yaml file from the "lang" folder
     String jsonString =
-        await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
-    Map<String, dynamic> jsonMap = json.decode(jsonString);
+        await rootBundle.loadString('assets/lang/${locale.languageCode}.yaml');
 
-    _localizedStrings = jsonMap.map((key, value) {
+    final mapData = loadYaml(jsonString);
+
+    _localizedStrings = mapData.map((key, value) {
       return MapEntry(key, value.toString());
     });
 
     return true;
   }
+
+  ///JSON
+  // Future<bool> load() async {
+  //     // Load the language JSON file from the "lang" folder
+  //     String jsonString =
+  //         await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
+  //     Map<String, dynamic> jsonMap = json.decode(jsonString);
+  //
+  //     _localizedStrings = jsonMap.map((key, value) {
+  //       return MapEntry(key, value.toString());
+  //     });
+  //
+  //     return true;
+  //   }
 
   // This method will be called from every widget which needs a localized text
   String translate(String key) {
